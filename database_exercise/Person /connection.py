beedb80bb335c2995cd9bbbbecc3dba6f98be6e8
poulstar <info@poulstar.com>
2021-02-14 14:31:07 +0330

    26Bahman

--- database_exercise/Person /connection.py
+++ database_exercise/Person /connection.py
@@ -1,4 +1,20 @@
 import pymysql
 
 
-class Connect()
+class Connect_to():
+
+    def connect_to_database(self):
+        try:
+            cn = pymysql.Connect(host="localhost", user="poulstar",
+                                 password="poulstar", db="information")
+            cr = cn.cursor()
+            print("connected")
+        except:
+            print("not connected")
+        finally:
+            print("I'm a programmer")
+
+        return cn, cr
+
+
+Connect_to().connect_to_database()
